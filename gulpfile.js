bfe99cf0918e5c8f7d9941b53f8ec49075e376bf
Steven Spriggs <steven.spriggs@gmail.com>
2014-11-05 17:01:09 -0500

    use handle bars instead of gulp-inject

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,11 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     prefix = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
-    inject = require("gulp-inject"),
     del = require("del"),
     runSequence = require('run-sequence'),
-    scsslint = require('gulp-scss-lint');
+    scsslint = require('gulp-scss-lint'),
+    handlebars = require('gulp-compile-handlebars');
 
-// Replaced with node del 
-// gulp.task('clean', function () {
-//   return gulp.src('./build/*', { read: false })
-//     .pipe(clean());
-// });
-// https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
 gulp.task('clean', function(cb){
   del([
     'build/**/*'
@@ -29,14 +23,9 @@ gulp.task('scss-lint', function() {
     .pipe(scsslint.failReporter());
 });
 
-gulp.task('rename-scss-partial',function(){
+gulp.task('compile-scss', function(){
   return gulp.src('./src/scss/_wvu-masthead__logo.scss')
     .pipe(rename('wvu-masthead__logo.scss'))
-    .pipe(gulp.dest('./build/scss/'));
-});
-
-gulp.task('compile-css', ['rename-scss-partial'], function(){
-  return gulp.src('./build/scss/*.scss')
     .pipe(sass({
       includePaths: ['scss'],
       outputStyle: 'expanded'
@@ -45,28 +34,21 @@ gulp.task('compile-css', ['rename-scss-partial'], function(){
     .pipe(gulp.dest('./build/css/'));
 });
 
-gulp.task('copy-test-html',function(){
-  return gulp.src('./src/test/index.html')
-    .pipe(gulp.dest('./build/'))
-})
-
-gulp.task('inject-src', ['scss-lint','copy-test-html','compile-css'], function () {
-  var target = gulp.src('./build/index.html');
-  target.pipe(inject(gulp.src(['./build/css/*.css'], {read: false}), {relative: true}))
-    .pipe(gulp.dest('./build/'));
-    
-  return target.pipe(inject(gulp.src(['./src/html/*.html']), {
-    starttag: '<!-- inject:{{ext}} -->',
-    transform: function (filePath, file) {
-      // return file contents as string
-      return file.contents.toString('utf8')
-    }
-  }))
-  .pipe(gulp.dest('./build/'))
+gulp.task('compile', ['scss-lint','compile-scss'], function () {
+  var templateData = require('./src/handlebars/data/_wvu-masthead__logo.json');
+  var options = {
+    batch : [
+      './src/handlebars'
+    ]
+  }
+  return gulp.src('./src/handlebars/test/index.hbs')
+        .pipe(handlebars(templateData, options))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('build',function(){
-  runSequence('clean','inject-src');
+  runSequence('clean','compile');
 });
 
 
